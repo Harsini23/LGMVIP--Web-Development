519c26eafa8eca403e943a1d1f356ed8c96cc0a2
Harsini <66863440+Harsini23@users.noreply.github.com>
2021-08-17 14:32:24 +0530

    final commit

--- task 2 react app/src/App.js
+++ task 2 react app/src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import axios from "axios";
 import Loading from "./Loading";
 import './mycomponents/Footer.css';
-import logo from "./mylogo.jpg";
+import logo from "./images.jpeg";
+
 
 const App = () => {
   const [cardData, setData] = useState([]);
@@ -45,15 +46,15 @@ const App = () => {
           <img
             src={myuser.avatar}
            
-            style={{height:"150px",width:"150px"}}
+            style={{height:"150px",width:"150px",border:"2px solid black"}}
           /><br/><br/>
-          <h4>{myuser.first_name}</h4>
-          <h4>{myuser.last_name}</h4>
-          <h4>{myuser.email}</h4>
+          <h4 style={{backgroundColor: "#FFDEDE",margin:"15px",padding:"4px",borderRadius:"30px"}}>{myuser.first_name}</h4>
+          <h4 style={{backgroundColor: "#FFDEDE",margin:"15px",padding:"4px",borderRadius:"30px"}}>{myuser.last_name}</h4>
+          <h4 style={{backgroundColor: "#FFDEDE",margin:"15px",padding:"4px",borderRadius:"30px"}}>{myuser.email}</h4>
           </div><br/>
           <button 
-          
-          style={{padding:"20px",backgroundColor:"#2C394B",color:"white"}}>
+          className="btn "
+          style={{padding:"20px",backgroundColor:"#2C394B",color:"white",border:"2px solid black",borderRadius:"30px"}}>
             Get to know more</button>
         </div>
       );
@@ -61,9 +62,9 @@ const App = () => {
   };
 
   return (
-    <div className="App">
+    <div className="App" >
       <nav className="navbar " style={{ backgroundColor: "#11324D",padding:"30px",margin:"30px" }}>
-        <img src={logo} style={{width:"100px",height:"100px"}} ></img>
+        <img src={logo} style={{width:"150px",height:"150px",padding:"20px"}} ></img>
         <h1 style={{color:"white"}}>Harsini</h1>
         <input style={{padding:"10px"}}
             type="text"
@@ -71,17 +72,17 @@ const App = () => {
             placeholder="Search Here"
         />
         <button
-          className="btn "
-          style={{ backgroundColor: "#D5EEBB" }}
+          className="btn"
+          style={{ backgroundColor: "#FDF5CA" }}
           onClick={display}>
           Get Users</button>
       </nav>
-      <body style={{backgroundColor:"#87A8A4",height:"2000px",margin:"20px"}}>
+      <body className="background" style={{padding:"50px",margin:"20px"}}>
         <div>
-          <h1 style={{padding:"35px"}}>Welcome to Home Page</h1><hr style={{width:"2px"}} ></hr>
+          <h1 style={{padding:"35px",color:"white",backgroundColor:"#2C394B"}}>Welcome to Home Page</h1><hr style={{width:"2px"}} ></hr>
 
       {loading ? <Loading /> : null}
-      <div className="container" style={userStyle}>
+      <div className="container" id="userStyle">
         {loading ? null : cardData.map(Card)}
       </div>
         </div>
@@ -92,13 +93,8 @@ const App = () => {
 };
 
 
-const userStyle = {
-  display: "grid",
-  gridGap: "2rem",
-  gridTemplateColumns: "repeat(3,1fr)",
-  marginTop:"30px",
-  marginBottom: "30px",
-};
+
+
 
 
 export default App;
